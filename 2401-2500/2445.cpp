af1ec408f6ecb68cd6548dc82fdbffe5fd719886
haneul99 <healing5746@gmail.com>
2020-09-22 18:20:57 +0900

    2445. 별 찍기 - 8

---
+++ 2401-2500/2445.cpp
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		for (int j = n - i + 1; j <= n; j++) {
+			printf("*");
+		}
+		for (int j = 2 * i - 1; j < 2 * n - 1; j++) {
+			printf(" ");
+		}//8 6 4 2 0
+		for (int j = n - i + 1; j <= n; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (int i = 1; i <= n - 1; i++) {
+		for (int j = i; j < n; j++) {
+			printf("*");
+		}
+		for (int j = 2*i; j > 0; j--) {
+			printf(" ");
+		}//2 4 6 8
+		for (int j = i; j < n; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+}
+
+/*
+2445. 별 찍기 - 8
+https://www.acmicpc.net/problem/2445
+
+문제
+예제를 보고 규칙을 유추한 뒤에 별을 찍어 보세요.
+
+입력
+첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+
+출력
+첫째 줄부터 2×N-1번째 줄까지 차례대로 별을 출력한다.
+
+*/
