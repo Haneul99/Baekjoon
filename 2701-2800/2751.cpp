97ff23f6af95e33df9f3770090c0219377d1d263
Haneul99 <healing5746@gmail.com>
2021-02-05 16:08:45 +0900

    2751. 수 정렬하기 2

--- 2701-2800/2751.cpp
+++ 2701-2800/2751.cpp
@@ -1,65 +1,21 @@
 #include<stdio.h>
-#include<string.h>
-#include<malloc.h>
+#include<algorithm>
 
-int *arr;
-
-int partition(int arr[], int left, int right) {
-	int low, high, pivot;
-	int tmp;
-	low = left;
-	high = right + 1;
-
-	pivot = (left + right) / 2;
-	tmp = arr[left];
-	arr[left] = arr[pivot];
-	arr[pivot] = tmp;
-	pivot = left;
-
-	do {
-		do {
-			low++;
-		} while (low <= right && arr[low] < arr[pivot]);
-		do {
-			high--;
-		} while (high >= left && arr[high] > arr[pivot]);
-		if (low < high) {
-			tmp = arr[low];
-			arr[low] = arr[high];
-			arr[high] = tmp;
-		}
-	} while (high > low);
-	tmp = arr[left];
-	arr[left] = arr[high];
-	arr[high] = tmp;
-	return high;
-}
-
-void quickSort(int arr[], int low, int high) {
-	if (high > low) {
-		int pivot = partition(arr, low, high);
-		quickSort(arr, low, pivot - 1);
-		quickSort(arr, pivot + 1, high);
-	}
-}
+using namespace std;
 
 int main() {
 	int n;
 	scanf("%d", &n);
-	arr = (int*)malloc(sizeof(int)*n);
+	int num[1000001];
 	for (int i = 0; i < n; i++) {
-		scanf("%d", &arr[i]);
+		scanf("%d", &num[i]);
 	}
-
-	quickSort(arr, 0, n-1);
-
+	sort(num, num + n);
 	for (int i = 0; i < n; i++) {
-		printf("%d\n",arr[i]);
+		printf("%d\n", num[i]);
 	}
 }
 
-// partition & quickSort
-// pivot을 middle index로 set
 
 /*
 2751. 수 정렬하기 2
