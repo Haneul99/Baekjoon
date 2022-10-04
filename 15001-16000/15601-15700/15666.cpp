83a32e8efff49487d5308038d03f4badf4c50a6a
haneulJeong <healing5746@gmail.com>
2022-10-04 20:37:45 +0900

    15666. N과 M (12)

---
+++ 15001-16000/15601-15700/15666.cpp
@@ -0,0 +1,71 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <limits.h>
+
+#define MAX 9
+
+using namespace std;
+
+map<string, int> permutations;
+int comb[MAX] = { 0, };
+vector<int> nums;
+
+int N, M; 
+
+void permutation(int idx, int depth) {
+	if (depth == M) {
+		string num = "";
+		for (int i = 0; i < depth; i++) {
+			num += to_string(comb[i]) + " ";
+		}
+		if (permutations.count(num) == 0) cout << num << '\n';	
+		permutations[num]++;
+	}
+	else {
+		for (int i = idx; i < N; i++) {
+			comb[depth] = nums[i];
+			permutation(i, depth + 1);
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		int x; cin >> x;
+		nums.push_back(x);
+	}
+	
+	sort(nums.begin(), nums.end());
+	permutation(0, 0);
+}
+
+
+/*
+15666. N과 M (12)
+https://www.acmicpc.net/problem/15666
+
+문제
+N개의 자연수와 자연수 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+
+N개의 자연수 중에서 M개를 고른 수열
+같은 수를 여러 번 골라도 된다.
+고른 수열은 비내림차순이어야 한다.
+길이가 K인 수열 A가 A1 ≤ A2 ≤ ... ≤ AK-1 ≤ AK를 만족하면, 비내림차순이라고 한다.
+입력
+첫째 줄에 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
+
+둘째 줄에 N개의 수가 주어진다. 입력으로 주어지는 수는 10,000보다 작거나 같은 자연수이다.
+
+출력
+한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
+
+수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+*/ 
